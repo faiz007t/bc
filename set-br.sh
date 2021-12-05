60bbb3943a28048014a6de43a9236838ed39ab66
faiz007t <faiziz18@gmail.com>
2021-12-05 19:41:47 +0800

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -2,9 +2,9 @@
 
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/Thebot119/mysc/master/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/Thebot119/mysc/master/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/Thebot119/mysc/master/restore.sh"
+wget -O autobackup "https://raw.githubusercontent.com/faiz007t/bc/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/faiz007t/bc/main/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/faiz007t/bc/main/restore.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x restore
